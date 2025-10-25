40fee8b0f700ca1aea2f6c2925911b6e6398b96f
quentinjd94 <quentinjdubois@gmail.com>
2025-10-25 22:44:55 +0200

    llm service

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,6 @@
 from .stt_service import stt_service
 from .tts_service import tts_service
-from .llm_service import llm_service, SYSTEM_PROMPT
+from .llm_service import llm_service
 
 __all__ = ["stt_service", "tts_service", "llm_service", "SYSTEM_PROMPT"]
 
