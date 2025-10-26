de1588636dc4202df415724ab446020aafd12b8e
quentinjd94 <quentinjdubois@gmail.com>
2025-10-26 23:31:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,8 @@ class Settings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
     
-    # Ollama
-    OLLAMA_URL: str = "http://localhost:11434"
-    OLLAMA_MODEL: str = "mistral:7b-instruct-q5_K_M"
-    OLLAMA_TIMEOUT: int = 30
+    #DEEPGRAM
+    DEEPGRAM_API_KEY: str = ""
     
     # OpenAI
     OPENAI_API_KEY: str = ""
